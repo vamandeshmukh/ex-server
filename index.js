88ca395aba391914ef1b78c03ae7df9c5ac8ae41
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-07-27 07:23:06 +0530

    Final Commit
    Final Commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import fun from './js-fun.js';
+// imports 
+import express from 'express'; // import from node_modules 
+import fun from './js-fun.js'; // import from our own scripts 
+
+// create constants with node_modules imports 
 const app = express();
 const port = 9999;
 
+// express use function for creating server  
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
+// getmapping requests 
 app.get('/', (req, res) => {
   console.log('Welcome!');
   res.send('Welcome to the app!');
@@ -21,6 +26,7 @@ app.get('/fun', (req, res) => {
   res.send(fun());
 });
 
+// console output after app starts 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
